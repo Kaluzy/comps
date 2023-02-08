b7ddd93303105772d0ce50ceba5a7d6e44c475c9
kaluzy <kaluzyt@gmail.com>
2023-02-08 11:53:20 -0500

    working on Acocordion icons

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
-
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
     // const content = isExpanded && <div>{item.content} </div>;
+    const icon = (
+      <sapn>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</sapn>
+    );
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.lable}</div>
-        <div>{isExpanded && <div>{item.content} </div>}</div>
+        <div
+          className="flex p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.lable}
+          {icon}
+        </div>
+        <div>
+          {isExpanded && <div className="border-b p-5">{item.content} </div>}
+        </div>
       </div>
     );
   });
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
